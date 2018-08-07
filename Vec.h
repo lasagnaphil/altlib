6fcbb7784e1faff1702d78e322397cdc85820570
Phillip Chang <lasagnaphil@snu.ac.kr>
2018-08-07 23:27:34 +0900

    second commit

--- Vec.h
+++ Vec.h
@@ -8,20 +8,35 @@
 #include <cassert>
 #include <cstring>
 
+#define VEC_FOREACH(__vec, __v, CODE) { \
+    for (int __i = 0; __i < __vec.size; ++__i) { \
+        auto& __v = __vec[__i]; \
+        CODE \
+    } \
+}
+
+#define VEC_FOREACHI(__vec, __v, __i, CODE) { \
+    for (int __i = 0; __i < __vec.size; ++__i) { \
+        auto& __v = __vec[__i]; \
+        CODE \
+    } \
+}
+
 template <typename T>
 struct Vec {
+
     T* data;
     size_t size;
     size_t capacity;
 
-    static Vec create(std::size_t capacity = 1) {
+    static Vec create(size_t capacity = 1) {
         Vec vec;
         vec.data = new T[capacity];
         vec.size = 0;
         vec.capacity = capacity;
         return vec;
     }
-    static Vec from(const T& elem, std::size_t count) {
+    static Vec fromElem(const T& elem, size_t count) {
         Vec vec;
         vec.data = new T[count];
         for (size_t i = 0; i < count; ++i) {
@@ -29,6 +44,13 @@ struct Vec {
         }
         vec.size = count;
         vec.capacity = count;
+        return vec;
+    }
+
+    static Vec fromRaw(T* data, size_t count) {
+        Vec vec;
+        memcpy(vec.data, data, sizeof(T) * count);
+        vec.size = count;
         return vec;
     }
 
@@ -57,22 +79,22 @@ struct Vec {
         return data[size];
     }
 
-    T& operator[](std::size_t idx) {
+    T& operator[](size_t idx) {
         assert(idx >= 0 && idx < size);
         T& item = data[idx];
         return item;
     }
 
-    const T& operator[](std::size_t idx) const {
+    const T& operator[](size_t idx) const {
         assert(idx >= 0 && idx < size);
         const T& item = data[idx];
         return item;
     }
 
-    void reserve(std::size_t newCapacity) {
+    void reserve(size_t newCapacity) {
         if (newCapacity <= capacity) { return; }
         T* newData = new T[newCapacity];
-        std::memcpy(newData, data, sizeof(T) * capacity);
+        memcpy(newData, data, sizeof(T) * capacity);
         delete[] data;
         data = newData;
         capacity = newCapacity;
@@ -82,10 +104,10 @@ struct Vec {
         shrink(size);
     }
 
-    void shrink(std::size_t newCapacity) {
+    void shrink(size_t newCapacity) {
         if (newCapacity >= capacity) { return; }
         T* newData = new T[newCapacity];
-        std::memcpy(newData, data, sizeof(T) * newCapacity);
+        memcpy(newData, data, sizeof(T) * newCapacity);
         delete[] data;
         data = newData;
         capacity = newCapacity;
